b8992eaaa5221d07fdaa7a3c8bc5d027c77995c3
terryCollinsHawkins <terry.collins-hawkins@outlook.com>
2022-11-18 15:36:43 +1300

    Actually removing the web projects now

--- SetGame.Api/Database/Read/QueryHandlers/IQueryHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SetGame.Api.Database.Read.QueryHandlers
-{
-    public interface IQueryHandler<T, U>
-    {
-        U Execute(T query);
-    }
-}
--- SetGame.Api/Database/Write/Commands/NewGameCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using SetGame.Set;
-
-namespace SetGame.Api.Database.Write.Commands
-{
-    public class NewGameCommand
-    {
-        public Game Game { get; set; }
-    }
-}
